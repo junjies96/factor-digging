37a2b8c313119f3b53a513b7943bf151f122df48
junjies96 <david1996@live.cn>
2017-07-26 22:47:01 +0800

    first factor

---
+++ 001_pe_ltm.py
@@ -0,0 +1,23 @@
+class UserSignal(Signal):
+
+	def __init__(self):
+		self._LRImportance = 0
+		self._treeImportance = 0
+		self._correlation = 0
+		self._decay = 0
+		self._annual_return = 0
+		self._annual_vol = 0
+		self._sharpe_ratio = 0
+		self._max_drawdown = 0
+		self._return_risk_ratio = 0
+		self._data_ret = 0
+
+    def dig(self, data):
+    
+        # Read data from Database
+        data.get_variable('AShareEODDerivativeIndicator', 's_val_pe_ttm')
+        # Deal with NaN value
+        data.deal_exception_value(('AShareEODDerivativeIndicator', 's_val_pe_ttm'), 'constant', 0)
+        # Get data after pre-process
+        pe = data.get_variable('AShareEODDerivativeIndicator', 's_val_pe_ttm')
+        return pe
